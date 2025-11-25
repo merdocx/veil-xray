d4241b024d11e198fc8d64f9661e458f49f23374
veil-xray <veil-xray@veil-bear.ru>
2025-11-25 09:35:53 +0300

    Update .gitignore to exclude IDE files

--- .cursor-server/bin/ef979b1b43d85eee2a274c25fd62d5502006e420/extensions/cursor-mcp/src/commands/registryAll.ts
+++
@@ -1,9 +0,0 @@
-import { ACTION_REGISTRY, ExtraContext } from './registry';
-import './mcpCommands'
-import { EverythingProviderArgs, ExtractArgs, ExtractReturn } from '@cursor/types';
-
-
-export function getCommandFromRegistry<K extends EverythingProviderArgs['name']>(commandName: K): ((args: ExtractArgs<K>, extraContext: ExtraContext) => ExtractReturn<K>) | undefined {
-	// @ts-ignore (Needed for gulpfile building only)
-	return ACTION_REGISTRY[commandName];
-}
